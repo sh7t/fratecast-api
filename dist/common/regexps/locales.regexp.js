83bf684d7b56da0a4cd1f31a30c92df3c86d0813
sh7t <karimgaldonsky@gmail.com>
2026-08-01 02:35:53 +0300

    fix: .idea added to .gitignore

--- dist/common/regexps/locales.regexp.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LOCALES_REGEXP = void 0;
-const locale_enum_1 = require("../enums/locale.enum");
-exports.LOCALES_REGEXP = new RegExp(`^(${Object.values(locale_enum_1.UserLocale).join('|')})$`);
-//# sourceMappingURL=locales.regexp.js.map
